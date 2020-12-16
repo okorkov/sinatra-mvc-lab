1c46b362e3fce32a3792a155ec656ef31dbea67c
okorkov <alex.okorkov@gmail.com>
2020-12-15 20:12:29 -0800

    commit tree 12085cb62d8c684b17251cc952301f3de8c0f73c

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do 
+    erb :user_input
+  end
+
+  post '/piglatinize' do 
+    @params = params
+    # @params.to_s
+    @pig = PigLatinizer.new
+    @pig.piglatinize(@params["user_phrase"])
+    # erb :output
+  end
+
 end
